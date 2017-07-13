f2ed3cb8550f2d41902bccedafb84f7f48e78440
Amira Trabelsi <at@transprog.org>
2017-07-13 11:49:19 +0100

    Use V8 syntax annd update constrains

--- project_role/models/inherited_hr_employee.py
+++ project_role/models/inherited_hr_employee.py
@@ -27,88 +27,72 @@
 # Checked out Version:   $LastChangedRevision$
 # HeadURL:               $HeadURL$
 # --------------------------------------------------------------------------------
-from openerp.osv import osv, fields
-from openerp.tools.translate import _
+from openerp import models, fields, api, _, tools
+from openerp.exceptions import ValidationError
 
-class hr_employee(osv.osv):
+class hr_employee(models.Model):
     _name = "hr.employee"
     _inherit = "hr.employee"
     
-    _columns = {
-                'user_id' : fields.many2one('res.users', required = True),
-                }
+    @api.one
+    @api.constrains('user_id')
+    def _check_user_employee_relation(self):
+        if not self.user_id :
+            raise ValidationError(_("Please select related user."))
+        elif len(self.user_id.employee_ids) >1:
+            raise ValidationError(_("Selected user already assigned to an other employee."))
+    
+#     @api.one
+#     @api.constrains('contract_ids') 
+#     def _check_employee_contract(self):
+#         if not self.contract_ids :
+#             raise ValidationError(_("Please select employee contract."))
 
-    def _check_user_employee(self, cr, uid, ids):
-        for employee in self.browse(cr, uid, ids) :
-            if employee.user_id.id is False :
-                return False
-            elif len(employee.user_id.employee_ids) >1 :
-                return False
-        return True
-    
-    _constraints = [
-                    (_check_user_employee, _("Selected user already assigned to an other employee "), ['user_id']),
-                    ]
-    
-    def name_get(self, cr, uid, ids, context = None) :
-        if context and context.has_key('current_model') and context['current_model'] == 'project.project' :
+    @api.multi
+    def name_get(self) :
+        if self._context and self._context.has_key('current_model') and self._context['current_model'] == 'project.project' :
             result = []
-            employee_names = super(hr_employee, self).name_get(cr, uid, ids, context)
-            for employee_id, employee_name in employee_names :
-                result.append((employee_id, employee_name + ', ' + str(self.browse(cr, uid, employee_id).availability)))
+            for employee in self:
+                result.append((employee.id, employee.name + ', ' + str(employee.availability)))
             return result
-        return super(hr_employee, self).name_get(cr, uid, ids, context)
+        return super(hr_employee, self).name_get()
     
-    def _get_working_hours_month_average(self, cr, uid, employee_id, context = None):
+    @api.multi
+    def _get_working_hours_month_average(self):
         from datetime import datetime, timedelta
-        employee = self.browse(cr, uid, employee_id, context)
-        employee_contract_id = employee.contract_id    # get only the last contract_id
+        self.ensure_one()
+        employee_contract_id = self.contract_id    # get only the last contract_id
         if employee_contract_id and employee_contract_id.working_hours :    # If the user has a contract
-            start_date = datetime.strptime(employee_contract_id.date_start, "%Y-%m-%d")
+            start_date = datetime.strptime(employee_contract_id.date_start, tools.DEFAULT_SERVER_DATE_FORMAT)
             date_to = start_date + timedelta(days = 365)
-            working_hours_month_average = int(round(self.pool.get('resource.calendar').interval_hours_get(cr, uid, employee_contract_id.working_hours.id, start_date, date_to) / 12))
+            working_hours_month_average = int(round(employee_contract_id.working_hours.interval_hours_get(start_date, date_to)[0] / 12))
         else :    # default working hours month average (8 hours/day, 20 working_days/month)
             working_hours_month_average = 160
         return working_hours_month_average
 
-    def _get_availability(self, cr, uid, ids, field_name, arg, context = None):
-        result = {}
-        for employee_id in ids :
+
+    def _get_availability(self):
+        for employee in self :
             # search only on open projects
-            project_member_ids = self.pool.get('project.member').search(cr, uid, [('employee_id', '=', employee_id), ('project_id.state', 'in', ['open'])], context = context)
-            project_members = self.pool.get('project.member').browse(cr, uid, project_member_ids, context)
+            project_members = self.env['project.member'].search([('employee_id', '=', employee.id), \
+                                                                 ('project_id.state', 'in', ['open'])])
             employee_effort = [member.hours_planned_monthly for member in project_members]
-            availability = self._get_working_hours_month_average(cr, uid, employee_id, context)
-            result[employee_id] = availability - sum(employee_effort)
-        return result
+            employee.availability = employee._get_working_hours_month_average() - sum(employee_effort)
 
-    _columns = {
-                'availability' : fields.function(_get_availability, type = 'integer', string = 'Availability (Hours/Month)', readonly = True),
-                'assigned_role_ids' : fields.one2many('project.member', 'employee_id', string = 'Assigned Roles'),
-                }
-
-    def create(self, cr, uid, values, context = None):
-        emply_id =  super(hr_employee, self).create(cr, uid, values, context)
-        if not self._check_user_employee(cr, uid, [emply_id]) :
-            raise osv.except_osv(_('Operation denied !'), _('Selected user already assigned to an other employee'))
-        return emply_id
+    user_id = fields.Many2one('res.users', required = True)
+    contract_ids = fields.One2many('hr.contract', 'employee_id', 'Contracts', required = True)
+    availability = fields.Integer(compute = _get_availability, string = 'Availability (Hours/Month)', readonly = True)
+    assigned_role_ids = fields.One2many('project.member', 'employee_id', string = 'Assigned Roles')
     
-hr_employee()
-
-class res_users(osv.osv):
+class res_users(models.Model):
     _name = "res.users"
     _inherit = "res.users"
     
-    def _check_user_employees(self, cr, uid, ids):
-        for user in self.browse(cr, uid, ids) :
-            if len(user.employee_ids) > 1 :
-                return False
-        return True
-    
-    _constraints = [
-                    (_check_user_employees, _("A user must be related to at most one employee"), ['employee_ids']),
-                    ]
+    @api.one
+    @api.constrains('employee_ids')
+    def _check_user_employees(self):
+        if len(self.employee_ids) > 1 :
+            raise ValidationError(_("A user must be related to at most one employee"))
 
-res_users()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4
 #eof $Id$
